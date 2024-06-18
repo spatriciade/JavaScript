afff3901aa7af5ff8ebfd423527ee8f50d298dbc
Patricia <spatriciade@hotmail.com>
2024-06-18 11:28:58 +0200

    More JS

---
+++ JS MOD 3/SESION 20/js/app.js
@@ -0,0 +1,91 @@
+//let result;
+//result = "Hello world!";
+//result = result.length;
+//result = result.split(" ");
+//result = result.replace(" ","%");
+//result= result.includes("W");
+
+
+//document.getElementById("app").innerHTML = result;
+//console.log(result);
+//remove all blank spaces
+//all chars lower case
+//replace vowels with numbers
+//e for 3 and with 0 (zero)
+//let result= "Hello World";
+//result = result.replaceAll("o","0");
+//result = result.replaceAll(" ","");
+//result = result.replaceAll("e","3");
+//result = result.toLowerCase();
+
+
+//document.getElementById("app").innerHTML = result;
+//console.log(result);
+
+//let result = "123";
+//result= Number(result);aqui suma 123+5
+//result = result + 5; // aqui concatena 1235
+//document.getElementById("app").innerHTML = result;
+//console.log(result);
+
+//let result = "Hello";
+//result += " world";
+//result = result + 5; // aqui concatena 1235
+//document.getElementById("app").innerHTML = result;
+//console.log(result);
+
+//const result= "123.45";
+//let num = Number(result);
+//
+//num = Math.floor(num);
+////document.getElementById("app").innerHTML = num;
+//console.log(num);
+
+//const result= "123.45";
+//let num = Number(result);
+//
+//num = Math.floor(num);
+//num = Math.ceil(num);//hacia arriba el redondeo
+//num = Math.floor(Math.random() * 100); // hacia abajo el redondeo
+
+////document.getElementById("app").innerHTML = num;
+//console.log(num);
+// Ejercicio 1. Escribe código en Javascript para que muestre en la web de nuestro proyecto el siguiente saludo con
+// un nombre de forma que el nombre sea una variable:
+// “Hello <name>, how are you doing today?”
+// Definir la variable con el nombre
+//let name = "John";
+// Crear el mensaje de saludo
+//let greetingMessage = `Hello ${name}, how are you doing today?`;
+//console.log(greetingMessage);
+
+// Ejercicio 2. Escribe código en Javascript para que determine si un número variable es par o impar mostrando en
+// nuestra web el resultado en un mensaje como este:
+// “Number 34 is even” ó “Number 13 is odd”
+
+// Definir la variable con el número a evaluar
+let number = 13; // Puedes cambiar este número para probar diferentes valores
+
+// Determinar si el número es par o impar
+let message;
+if (number % 2 === 0) {
+    message = `Number ${number} is even`;
+} else {
+    message = `Number ${number} is odd`;
+}
+
+console.log(message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
